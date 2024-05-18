cbacc2d60db4feb56ae73e78953437c8dda26609
Thiago NeveS <140116209+Thfneves@users.noreply.github.com>
2024-05-18 13:41:39 -0300

    v1

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    
+    var leftDiceNumber  = 1
+    var rightDiceNumber  = 5
+    
+    
+
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+
+       
+        diceImageView1.image = UIImage(named: ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"][Int.random(in: 0...5)])
+        diceImageView2.image = UIImage(named: ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"][Int.random(in: 0...5)])
+     
+
+    }
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+
+var a = 5
+var b = 8
+var c = 0
+
+c = b
+b = a
+a = c
+
+
+print (a)
+print (b)
